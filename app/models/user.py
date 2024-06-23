b13930a438d4fce95d5ad7e4ef306df753a310d7
Raman <mongaraman0414@icloud.com>
2024-06-23 23:09:07 +0100

    Fixed app errorsn.

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,9 @@
 from ..db import db
 from flask_login import UserMixin
 
+# Use str instead of text_type
+text_type = str
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
